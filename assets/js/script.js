ba39c1f3a7af28bb6c0753f19a2e1c59e4c60873
yeasin13 <yeasinarafat8001@gmail.com>
2022-10-25 18:01:08 +0600

    footer-done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // add event on element
 
 const addEventOnElem = function(elem, type, callback) {
-    if(elem,length >1){
+    if(elem.length >1){
         for(let i=0; i<elem.length; i++);
     }
     else {
@@ -35,15 +35,20 @@ const closeNavbar = function() {
 addEventOnElem(navbarLinks, "click", closeNavbar);
 
 
-// active header when windows scroll down to 100px
+// active header &back top btn when windows scroll down to 100px
+
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
 const activeElemOnScroll = function () {
     if(window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     }
     else{
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
 
